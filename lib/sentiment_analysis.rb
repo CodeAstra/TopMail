79dada5812d6bab6e08d7549aed5f939f5fe2522
radhika <sparkx.radhika@gmail.com>
2015-05-31 01:50:23 +0530

    changed sentiment analysis from helpers to lib

--- lib/sentiment_analysis.rb
+++ lib/sentiment_analysis.rb
@@ -2,7 +2,7 @@ require 'httparty'
 
 module SentimentAnalysis
     def calculate_sentiment(text)
-        response = HTTParty.get('https://api.idolondemand.com/1/api/sync/analyzesentiment/v1', {query: {text:text, apikey: 'd55419b0-1eec-4957-a93d-aabeb11bf0c2'}})
+        response = HTTParty.get('https://api.idolondemand.com/1/api/sync/analyzesentiment/v1', {query: {text:text, apikey: SENTIMENT_ANALYSIS_API_KEY}})
 
 
         response_hash = JSON.parse(response.to_json)
