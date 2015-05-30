3b19c8e656ca4f35419fbf6e39ac733900889bf7
radhika <sparkx.radhika@gmail.com>
2015-05-31 02:12:13 +0530

    Sentiment Analysis module in lib working

--- lib/sentiment_analysis.rb
+++ lib/sentiment_analysis.rb
@@ -1,15 +1,21 @@
 require 'httparty'
 
 module SentimentAnalysis
-    def calculate_sentiment(text)
-        response = HTTParty.get('https://api.idolondemand.com/1/api/sync/analyzesentiment/v1', {query: {text:text, apikey: SENTIMENT_ANALYSIS_API_KEY}})
+  def self.calculate(text)
+    response = HTTParty.get(
+      'https://api.idolondemand.com/1/api/sync/analyzesentiment/v1',
+      {
+        query: {
+          text:text,
+          apikey: ENV['SENTIMENT_ANALYSIS_API_KEY']
+        }
+      }
+    )
 
+    response_hash = JSON.parse(response.to_json)
 
-        response_hash = JSON.parse(response.to_json)
-
-        aggregate_sentiment = response_hash['aggregate']['sentiment']
-        aggregate_score =  response_hash['aggregate']['score']
-        return {aggregate_sentiment : aggregate_sentiment ,
-                aggregate_score : aggregate_score}
-    end
+    aggregate_sentiment = response_hash['aggregate']['sentiment']
+    aggregate_score =  response_hash['aggregate']['score']
+    return [aggregate_sentiment, aggregate_score]
+  end
 end
