c380057689154cdf45da6d20d54efede28cdc6d1
radhika <sparkx.radhika@gmail.com>
2015-05-31 13:14:43 +0530

    UI Fixes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'home/settings' =>'home#settings'
+  resources :emails
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- lib/sentiment_analysis.rb
+++ lib/sentiment_analysis.rb
@@ -1,68 +1,68 @@
-# require 'httparty'
-
-# module SentimentAnalysis
-#   def self.calculate(text)
-#     response = HTTParty.get(
-#       'https://api.idolondemand.com/1/api/sync/analyzesentiment/v1',
-#       {
-#         query: {
-#           text:text,
-#           apikey: ENV['SENTIMENT_ANALYSIS_API_KEY']
-#         }
-#       }
-#     )
-
-#     response_hash = JSON.parse(response.to_json)
-
-#     aggregate_score =  response_hash['aggregate']['score']
-#     return aggregate_score
-#   end
-# end
-
 require 'httparty'
 
 module SentimentAnalysis
-
-  texts = [
-    "I am a pathetic person",
-    "Today is going to be a good day",
-    "I wish you were dead"
-  ]
-
-  def self.calculate(texts)
-    actions = []
-    for text in texts
-      actions.push(
-        {
-          name:"analyzesentiment",
-          version:"v1",
-          params:{
-            text:text
-          }
-        }
-      )
-    end
-    job_id = HTTParty.post(
-      'https://api.idolondemand.com/1/job',
-      
-        {
-          actions: actions,
-          apikey: "ae974106-b56e-425a-8de6-9290ec2ec4c8"
-        }
-      
-    )
-
-    puts job_id.response.inspect
-
-    result = HTTParty.get(
-      'https://api.idolondemand.com/1/job/result/' + job_id,
+  def self.calculate(text)
+    response = HTTParty.get(
+      'https://api.idolondemand.com/1/api/sync/analyzesentiment/v1',
       {
         query: {
-          apikey: ENV['SENTIMENT_ANALYSIS_API_KEY'],
+          text:text,
+          apikey: ENV['SENTIMENT_ANALYSIS_API_KEY']
         }
       }
     )
 
-    puts result.inspect
+    response_hash = JSON.parse(response.to_json)
+
+    aggregate_score =  response_hash['aggregate']['score']
+    return aggregate_score
   end
 end
+
+# require 'httparty'
+
+# module SentimentAnalysis
+
+#   texts = [
+#     "I am a pathetic person",
+#     "Today is going to be a good day",
+#     "I wish you were dead"
+#   ]
+
+#   def self.calculate(texts)
+#     actions = []
+#     for text in texts
+#       actions.push(
+#         {
+#           name:"analyzesentiment",
+#           version:"v1",
+#           params:{
+#             text:text
+#           }
+#         }
+#       )
+#     end
+#     job_id = HTTParty.post(
+#       'https://api.idolondemand.com/1/job',
+      
+#         {
+#           actions: actions,
+#           apikey: "ae974106-b56e-425a-8de6-9290ec2ec4c8"
+#         }
+      
+#     )
+
+#     puts job_id.response.inspect
+
+#     result = HTTParty.get(
+#       'https://api.idolondemand.com/1/job/result/' + job_id,
+#       {
+#         query: {
+#           apikey: ENV['SENTIMENT_ANALYSIS_API_KEY'],
+#         }
+#       }
+#     )
+
+#     puts result.inspect
+#   end
+# end
